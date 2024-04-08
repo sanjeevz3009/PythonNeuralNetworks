32c4ad517b811d63d358a3a088cfa6c41a72034a
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-08 23:02:55 +0100

    Model saved, training complete and testing complete

--- nn_text_classification.py
+++ nn_text_classification.py
@@ -25,32 +25,55 @@ def decode_review(text):
 
 # print(decode_review(test_data[0]))
 
-# model
-model = keras.Sequential()
-model.add(keras.layers.Embedding(88000, 16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation="relu"))
-model.add(keras.layers.Dense(1, activation="sigmoid"))
+# # model
+# model = keras.Sequential()
+# model.add(keras.layers.Embedding(88000, 16))
+# model.add(keras.layers.GlobalAveragePooling1D())
+# model.add(keras.layers.Dense(16, activation="relu"))
+# model.add(keras.layers.Dense(1, activation="sigmoid"))
 
-# print(len(test_data[0]), len(test_data[1]))
+# # print(len(test_data[0]), len(test_data[1]))
 
-model.summary()
+# model.summary()
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+# model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
-x_val = train_data[:10000]
-x_train = train_data[10000:]
+# x_val = train_data[:10000]
+# x_train = train_data[10000:]
 
-y_val = train_labels[:10000]
-y_train = train_labels[10000:]
+# y_val = train_labels[:10000]
+# y_train = train_labels[10000:]
 
-fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+# fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
 
-results = model.evaluate(test_data, test_labels)
+# results = model.evaluate(test_data, test_labels)
 
-print(results)
+# print(results)
 
-model.save("model.h5")
+# model.save("model.h5")
+
+def review_encode(s):
+    encoded = [1]
+    
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+    
+    return encoded
+
+model = keras.models.load_model("model.h5")
+
+with open("review_text_2.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
 
 
 # test_review = test_data[0]
